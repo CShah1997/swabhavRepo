4efc697bc7ba1bec4b294c42f988fe3b2a47956e
CShah1997 <chiragshahindia18@gmail.com>
2019-09-12 12:30:40 +0530

    Send Status Code

--- PROJECT_TMS/controllers/UserController.js
+++ PROJECT_TMS/controllers/UserController.js
@@ -10,20 +10,20 @@ class UserController
         app.get('/api/v1/users',function(request,response){
             userServices.getAllUsers()
             .then(function(result){
-                response.send(result)
+                response.status(200).send(result)
             })
             .catch(function(err){   
-                response.send(err)
+                response.status(404).send(err)
             })
         })
 
         app.get('/api/v1/users/:id',function(request,response){
             userServices.getUserById(request.params.id)
             .then(function(result){
-                response.send(response)
+                response.status(200).send(response)
             })
             .catch(function(err){
-                response.send(err)
+                response.status(404).send(err)
             })
         })
 
@@ -31,10 +31,10 @@ class UserController
             let dataObj = request.body;
             userServices.registerUser(dataObj)
             .then(function(result){
-                response.send(result)
+                response.status(200).send(result)
             })
             .catch(function(err){
-                response.send(err)
+                response.status(404).send(err)
             })
         })
 
@@ -49,20 +49,20 @@ class UserController
             let task = request.body.task;
             userServices.updateUser(id,firstName,lastName,mobileNumber,email,userName,password)
             .then(function(result){
-                response.send(result)
+                response.status(200).send(result)
             })
             .catch(function(err){
-                response.send(err)
+                response.status(404).send(err)
             })
         })
 
         app.delete('/api/v1/users/:id',function(request,response){
             userServices.deleteUser(request.params.id)
             .then(function(result){
-                response.send(result)
+                response.status(200).send(result)
             })
             .catch(function(err){
-                response.send(err)
+                response.status(404).send(err)
             })
         })
 
@@ -72,10 +72,10 @@ class UserController
             userServices.authenticate(userName,password)
             .then(function(result){
                 console.log("Welcome...")
-                response.send(result)
+                response.status(200).send(result)
             })
             .catch(function(err){
-                response.send(err)
+                response.status(404).send(err)
             })
         })
 
